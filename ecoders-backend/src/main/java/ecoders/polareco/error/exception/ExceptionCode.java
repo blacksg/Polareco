bef08331c3ba8e24c8c797e62fc5023ce4c1f9cc
blacksg <sagwa.nth@gmail.com>
2024-02-17 17:07:16 +0900

    update: 에러에 대응하는 HTTP 응답 코드 수정

--- ecoders-backend/src/main/java/ecoders/polareco/error/exception/ExceptionCode.java
+++ ecoders-backend/src/main/java/ecoders/polareco/error/exception/ExceptionCode.java
@@ -10,19 +10,19 @@ import static org.springframework.http.HttpStatus.*;
 @Getter
 public enum ExceptionCode {
 
-    MEMBER_ALREADY_EXISTS(FORBIDDEN, "Member already exists"),
+    MEMBER_ALREADY_EXISTS(CONFLICT, "Member corresponding to the email already exists"),
     EMAIL_VERIFICATION_CODE_NOT_FOUND(NOT_FOUND, "Email verification code not found"),
-    EMAIL_VERIFICATION_CODE_MISMATCH(UNAUTHORIZED, "Email verification code mismatch"),
+    EMAIL_VERIFICATION_CODE_MISMATCH(FORBIDDEN, "Email verification code mismatch"),
     MEMBER_NOT_FOUND(NOT_FOUND, "Member not found"),
     LOGIN_FAILED(UNAUTHORIZED, "Login failed"),
-    JWT_EXPIRED(FORBIDDEN, "JWT expired"),
-    JWT_INVALID(FORBIDDEN, "JWT invalid"),
-    REFRESH_TOKEN_INVALID(FORBIDDEN, "Refresh token invalid"),
+    JWT_EXPIRED(FORBIDDEN, "Expired JWT"),
+    JWT_INVALID(FORBIDDEN, "Invalid JWT"),
+    REFRESH_TOKEN_INVALID(FORBIDDEN, "Invalid refresh token"),
     REFRESH_TOKEN_NOT_FOUND(NOT_FOUND, "Refresh token not found"),
     MAIL_MESSAGING_FAILED(INTERNAL_SERVER_ERROR, "Mail messaging failed"),
     PASSWORD_RESET_TOKEN_NOT_FOUND(NOT_FOUND, "Password reset token not found"),
     PASSWORD_RESET_TOKEN_MISMATCH(FORBIDDEN, "Password reset token mismatch"),
-    PASSWORD_MISMATCH(FORBIDDEN, "Password mismatch"),
+    PASSWORD_MISMATCH(UNAUTHORIZED, "Password mismatch"),
     IMAGE_UPLOAD_FAILED(INTERNAL_SERVER_ERROR, "Image upload failed"),
     POST_NOT_FOUND(NOT_FOUND, "Post not found"),
     CANNOT_CHANGE_POST(FORBIDDEN, "Post cannot change"),
@@ -30,7 +30,6 @@ public enum ExceptionCode {
     COMMENT_NOT_FOUND(NOT_FOUND, "Comment not found"),
     CANNOT_CHANGE_COMMENT(FORBIDDEN, "Comment cannot change"),
     CANNOT_DELETE_COMMENT(FORBIDDEN, "Comment cannot delete");
-
 
     private final HttpStatus httpStatus;
 
